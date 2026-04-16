bd2bd855c2ac1244650ae517aff22971f8622c73
IvySul <isulliv5@vols.utk.edu>
2026-04-15 23:34:58 -0400

    a

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const screens = Array.from(document.querySelectorAll(".screen"));
 const navButtons = Array.from(document.querySelectorAll("[data-go]"));
 const tabButtons = Array.from(document.querySelectorAll(".tab-bar button"));
+const tabBar = document.querySelector(".tab-bar");
+
+const tabVisibleScreens = new Set([
+  "screen-matches",
+  "screen-map",
+  "screen-auth",
+  "screen-profile",
+]);
 
 function showScreen(screenId) {
   screens.forEach((screen) => {
@@ -10,6 +18,10 @@ function showScreen(screenId) {
   tabButtons.forEach((button) => {
     button.classList.toggle("active", button.dataset.go === screenId);
   });
+
+  if (tabBar) {
+    tabBar.classList.toggle("hidden", !tabVisibleScreens.has(screenId));
+  }
 }
 
 navButtons.forEach((button) => {
